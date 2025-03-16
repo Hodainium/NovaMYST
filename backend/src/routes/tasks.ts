cc5284763da0ece9aee707b2973521c321c44492
Caden Yin <cadenyin@MacBookAir.fios-router.home>
2025-03-16 03:56:19 -0400

    implemented TypeScript but database calls not working

---
+++ backend/src/routes/tasks.ts
@@ -0,0 +1,29 @@
+const express = require('express');
+const {
+  createTask,
+  getTasks,
+  updateTask,
+  deleteTask,
+  calculateReward,
+  registerUser,
+  loginUser,
+  testdb, 
+} = require('../controllers/taskController'); // one dot -> you are in the current directory two dot -> you go into dist
+
+const router = express.Router();
+
+// Task routes
+router.post('/create', createTask);
+router.get('/list', getTasks);
+router.put('/update/:id', updateTask);
+router.delete('/delete/:id', deleteTask);
+
+// User registration route
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/test-firestore', testdb);
+
+// Reward calculation route
+router.post('/calculateReward', calculateReward);
+
+module.exports = router;
