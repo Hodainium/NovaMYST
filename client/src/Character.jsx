ea5c58a883360d63e530cb406161c07b192140d3
magjeck <alexwuliu8456@gmail.com>
2025-05-04 12:07:07 -0400

    Character model working

--- client/src/Character.jsx
+++ client/src/Character.jsx
@@ -1,29 +1,41 @@
 import "./Character.css";
 import {useState} from 'react';
+import React from 'react'; //
+import male from './assets/male_model.png'
+import female from './assets/female_model.png'
+import knightHelmet from './assets/knightHelmetModel.png'
+import knightChest from './assets/knightChestModel.png'
+import knightPants from './assets/knightPantsModel.png'
+import knightShoes from './assets/knightShoesModel.png'
 
 function Character() {
-const [hat, setHat] = useState('default');
-const [top, setTop] = useState('default');
-const [bottom, setBottom] = useState('default');
-const [shoes, setShoes] = useState('default');
+const [gender, setGender] = useState("Male");
+const [hat, setHat] = useState('');
+const [top, setTop] = useState('');
+const [bottom, setBottom] = useState('');
+const [shoes, setShoes] = useState('');
 
-    // Setting the clothes
-    const toggleSetHat = async(e) => {
+const changeGender = (SelectedGender) => {
+    setGender(SelectedGender);
+}
 
-    };
+const changeHat = (SelectedHat) => {
+    setHat(SelectedHat);
+}
 
-    const toggleSetTop = async(e) => {
+const changeTop = (SelectedTop) => {
+    setTop(SelectedTop);
+}
 
-    };
+const changeBottom = (SelectedBottom) => {
+    setBottom(SelectedBottom);
+}
 
-    const toggleSetBottom = async(e) => {
+const changeShoes = (SelectedShoes) => {
+    setShoes(SelectedShoes);
+}
 
-    };
-
-    const toggleSetShoes = async(e) => {
-
-    };
-
+// Shoes first, then pants, then helmet, then armor
 
     return (
         <div className="background"> 
@@ -31,9 +43,24 @@ const [shoes, setShoes] = useState('default');
                 <h1 className="characterTitle">Character Screen</h1>
 
                 <div className="gender">
-                    <button className="genderButton"> Male </button>
-                    <button className="genderButton"> Female </button>
+                    <button className="genderButton" onClick={() => changeGender("Male")}> Male </button>
+                    <button className="genderButton" onClick={() => changeGender("Female")}> Female </button>
                 </div>
+
+                <div className="CharacterModel">
+                    {gender === "Male" && (<img className="maleModel" src={male} alt="Male" />)}
+                    {gender === "Female" && (<img className="femaleModel" src={female} alt="Female"/>)}
+
+                    {shoes === "knight" && (<img className="knightShoesModel" src={knightShoes} alt="Knight Shoes"/>)}
+                    
+                    {bottom === "knight" && (<img className="knightBottomModel" src={knightPants} alt="Knight Pants"/>)}
+
+                    {top === "knight" && (<img className="knightTopModel" src={knightChest} alt="Knight Chest"/>)}
+
+                    {hat === "knight" && (<img className="knightHelmetModel" src={knightHelmet} alt="Knight Helmet"/>)}
+                </div>
+                
+
             </div>
 
             <div className="wardrobe">
@@ -41,21 +68,21 @@ const [shoes, setShoes] = useState('default');
 
                 <div className="clothingSelection">
 
-                <h3 className="defaultTitle"> Default Set </h3>
-                    <div className="defaultSet"> 
-                        <button className="default_hat">  </button>
-                        <button className="default_top"> </button>
-                        <button className="default_bottom"> </button>
-                        <button className="default_shoes"> </button>
+                <h3 className="defaultTitle">Default</h3>
+                    <div className="default"> 
+                        <button className="defaultHat" onClick={() => changeHat('')}>  </button>
+                        <button className="defaultTop" onClick={() => changeTop('')}>  </button>
+                        <button className="defaultBottom" onClick={() => changeBottom('')}>  </button>
+                        <button className="defaultShoes" onClick={() => changeShoes('')}>  </button>
                     </div>
                     
 
                     <h3 className="knightTitle"> Knight Set </h3>
                     <div className="knightSet">
-                        <button className="knight_helmet">  </button>
-                        <button className="knight_chest">  </button>
-                        <button className="knight_leggings">  </button>
-                        <button className="knight_boots">  </button>
+                        <button className="knight_helmet" onClick={() => changeHat('knight')}>  </button>
+                        <button className="knight_chest" onClick={() => changeTop('knight')}>  </button>
+                        <button className="knight_leggings" onClick={() => changeBottom('knight')}>  </button>
+                        <button className="knight_boots" onClick={() => changeShoes('knight')}>  </button>
                     </div>
 
                     <h3 className="princessTitle"> Princess Set </h3>
